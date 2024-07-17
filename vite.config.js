91c0ee332c676bf9ce47c1443b8025f8c9e11a57
Chun-Ming, Liao <97944035+monky6503@users.noreply.github.com>
2024-07-17 23:09:29 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base:'portfoliowebsite-Vue-',
+  base:'portfolioWebsite-Vue-',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
