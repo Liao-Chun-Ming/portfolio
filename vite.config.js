7a9561cc15ec3226dba8066759a3f8a01008e5a2
Chun-Ming, Liao <97944035+monky6503@users.noreply.github.com>
2024-10-18 13:34:13 +0800

    feat:update website name

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  base:'portfolioWebsite-Vue-',
+  plugins: [vue()],
+  base: 'portfolio',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  server:{
-    open:true
+  server: {
+    open: true
   }
 })
